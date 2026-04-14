86a8da72fabf812b1c3fe1e5fc2adb70b7dfd8a1
jmcaluag <josephcaluag@gmail.com>
2026-04-15 11:13:45 +1200

    Add zenmode plugin

--- init.lua
+++ init.lua
@@ -211,6 +211,7 @@ require('lazy').setup({
   require 'plugins.mini',
   require 'plugins.treesitter',
   require 'plugins.vimwiki',
+  require 'plugins.zenmode',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
