2c72a621578bcef448afc5f009253decc8813e12
jmcaluag <josephcaluag@gmail.com>
2026-04-14 13:10:12 +1200

    Remove additional config for vimwiki to use the .wiki extension

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -3,8 +3,6 @@ return {
   init = function()
     vim.g.vimwiki_list = { {
       path = '~/mOS/areas/vimwiki/',
-      syntax = 'markdown',
-      ext = '.md',
     } }
     vim.g.vimwiki_global_ext = 0 -- Important: Don't treat all MD files as vimwiki
   end,
